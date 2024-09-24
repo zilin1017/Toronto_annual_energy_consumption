41a1d3bb2f33c2dad75f870d90419694239c3876
Zilin Liu <liuzilin.liu@mail.utoronto.ca>
2024-09-24 01:39:43 -0400

    download data and named

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from  opendatatoronto
+# Author: Irene Liu
+# Date: 24 September 2024
+# Contact: liuzilin.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
