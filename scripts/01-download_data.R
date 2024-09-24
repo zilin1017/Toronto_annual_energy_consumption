fe1ef818db766db737b999d8b6733e86fc2f662d
Zilin Liu <liuzilin.liu@mail.utoronto.ca>
2024-09-24 01:37:49 -0400

    changed code and name the data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -10,17 +10,19 @@
 
 #### Workspace setup ####
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
-
+library(dplyr)
+library(readr)
+library(readxl)
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+consumption-data <- download.file("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/0600cad8-d024-483b-a9a8-ecfc3e32e375/resource/9353c7fe-d33d-47e9-9934-c6ca8bf04c75/download/annual-energy-consumption-data-2021.xlsx", 
+              destfile = "~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx", 
+              mode = "wb")
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+consumption_data <- read_excel("~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx")
+
+head(consumption_data)
 
          
