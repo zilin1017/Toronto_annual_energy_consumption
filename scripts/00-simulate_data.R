d6fc0e4253841bb6e55148b67582e947fe4cf639
Zilin Liu <liuzilin.liu@mail.utoronto.ca>
2024-09-24 13:48:37 -0400

    code updated

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from  opendatatoronto
+# Author: Irene Liu
+# Date: 24 September 2024
+# Contact: liuzilin.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
@@ -13,7 +12,17 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+my_data <- readxl::read_excel("data/raw_data/annual-energy-consumption-data-2021.xlsx", skip = 5) # 记得改
+my_data <- my_data|>
+  dplyr::select(c(`Portfolio Manager Property ID`, `Property GFA - Self-Reported (m²)`, `Electricity Use - Grid Purchase (kWh)`)) |>
+  dplyr::mutate(ID = `Portfolio Manager Property ID`,
+                Area = `Property GFA - Self-Reported (m²)`,
+                kWh = `Electricity Use - Grid Purchase (kWh)`) |>
+  dplyr::mutate(kWh = as.numeric(kWh)) |>
+  dplyr::filter(Area < 2500 & Area > 30) |>
+  dplyr::mutate(log_Area = log(Area))
+
+write.csv(my_data, "data/analysis_data/simulated_analysis_data.csv")
 
 
 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,10 +1,4 @@
-#### Preamble ####
-# Purpose: Downloads and saves the data from  opendatatoronto
-# Author: Irene Liu
-# Date: 24 September 2024
-# Contact: liuzilin.liu@mail.utoronto.ca
-# License: MIT
-# Pre-requisites: None
+
 
 
 #### Workspace setup ####
@@ -13,15 +7,15 @@ library(dplyr)
 library(readr)
 library(readxl)
 #### Download data ####
-consumption-data <- download.file("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/0600cad8-d024-483b-a9a8-ecfc3e32e375/resource/9353c7fe-d33d-47e9-9934-c6ca8bf04c75/download/annual-energy-consumption-data-2021.xlsx", 
+download.file("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/0600cad8-d024-483b-a9a8-ecfc3e32e375/resource/9353c7fe-d33d-47e9-9934-c6ca8bf04c75/download/annual-energy-consumption-data-2021.xlsx", 
               destfile = "~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx", 
               mode = "wb")
 
 
 #### Save data ####
 
-consumption_data <- read_excel("~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx")
+raw_data<- read_excel("~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx")
 
-head(consumption_data)
+head(raw_data)
 
-         
+write_csv(raw_data, "data/raw_data/raw_data.csv")       
