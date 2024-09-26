a384ed614271197b64b316aebd354004f533d5f1
Zilin Liu <liuzilin.liu@mail.utoronto.ca>
2024-09-26 17:37:54 -0400

    updated code

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -4,18 +4,30 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(dplyr)
-library(readr)
-library(readxl)
-#### Download data ####
-download.file("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/0600cad8-d024-483b-a9a8-ecfc3e32e375/resource/9353c7fe-d33d-47e9-9934-c6ca8bf04c75/download/annual-energy-consumption-data-2021.xlsx", 
-              destfile = "~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx", 
-              mode = "wb")
 
+#### Download data ####
+
+# get package
+package <- show_package("0600cad8-d024-483b-a9a8-ecfc3e32e375")
+package
+
+# get all resources for this package
+resources <- list_package_resources("0600cad8-d024-483b-a9a8-ecfc3e32e375")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
 #### Save data ####
 
-raw_data<- read_excel("~/Desktop/Toronto_annual_energy_consumption/data/raw_data/annual-energy-consumption-data-2021.xlsx")
+resource_id <- "9353c7fe-d33d-47e9-9934-c6ca8bf04c75"
+
+# Download the data
+raw_data <- get_resource(resource_id)
+
+# Save it as a CSV (if needed)
+write_csv(raw_data, "data/raw_data/raw_data.csv")
+
+# View the data
 
 head(raw_data)
 
-write_csv(raw_data, "data/raw_data/raw_data.csv")       
