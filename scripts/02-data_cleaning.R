d395e3047563a2121ed0651ce612b0c19f4e2a2d
Zilin Liu <liuzilin.liu@mail.utoronto.ca>
2024-09-26 22:54:56 -0400

    updated code

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,22 +9,23 @@
 #### Workspace setup ####
 library(tidyverse)
 library(dplyr)
+library(readr)
 
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/raw_data.csv", skip=5)
 
-cleaned_data <-
-  raw_data |>
-  dplyr::select(c(`Portfolio Manager Property ID`, `Property GFA - Self-Reported (m²)`, `Electricity Use - Grid Purchase (kWh)`),`Primary Property Type - Self Selected`) |>
+cleaned_data <-raw_data |>
+  dplyr::select(c(`Portfolio Manager Property ID`, `Property GFA - Self-Reported (m²)`, 
+`Electricity Use - Grid Purchase (kWh)`), `Primary Property Type - Self Selected`) |>
   dplyr::mutate(ID = `Portfolio Manager Property ID`,
                 Area = `Property GFA - Self-Reported (m²)`,
                 kWh = `Electricity Use - Grid Purchase (kWh)`,
                 Property_Type = `Primary Property Type - Self Selected` )|>
   dplyr::mutate(kWh = as.numeric(kWh)) |>
   dplyr::filter(Area < 2500 & Area > 30) |>
-  dplyr::mutate(log_Area = log(Area))
+  dplyr::mutate(log_Area = log(Area))|>
+  dplyr::arrange(ID)
 
 #### Save data ####
 write.csv(cleaned_data, "data/analysis_data/analysis_data.csv")
-
 
